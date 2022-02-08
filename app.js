55aac52831b2daf1d5c6a6734a64c6834ab7c794
666cocohappy <49433935+666cocohappy@users.noreply.github.com>
2022-02-08 20:24:49 +0800

    高鸿剑，七夕

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+App({
+  onLaunch() {
+
+  },
+  globalData: {
+    person: null,
+    sex: null,
+    id : null,
+    avatarUrl:null
+  }
+})
---
+++ envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-2gml49boac77c74c","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
